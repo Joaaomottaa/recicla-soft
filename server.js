c9aa2489d7465c7df7804bc1d2385b373d23f0ca
João <l2mortal01@gmail.com>
2025-05-03 01:28:44 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 // server.js
 
 const express = require('express');
@@ -399,4 +399,4 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server rodando em http://localhost:${PORT}`);
 });
->>>>>>> ef4e304 (Implementacao do site)
+
